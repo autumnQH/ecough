6739a7ba5215f5685d222cc4da000a653b55109c
MAC <mac@MACtekiMacBook-Air.local>
2017-08-22 11:14:17 +0800

    1

--- app/controllers/my.js
+++ app/controllers/my.js
@@ -48,43 +48,7 @@ var getPlay = async (ctx, next)=>{
 };
 
 var getOrder = async (ctx, next) => {
-    console.log('进来啦');
-    let code =  ctx.query.code;//获取网页授权code
-        var data = {
-            appid: config.weixin.appid, //appId
-            mch_id: config.wx.mchid, //商户号id
-            body: 'Test', //商品描述
-            nonce_str: tools.createRandom(),
-            //sign: data.sign,//签名
-            out_trade_no: tools.trade(),//商户订单号
-            total_fee: '1',//标价金额
-            attach: '支付测试',
-            spbill_create_ip: ctx.header['x-forwarded-for'],//终端IP
-            notify_url: '/getOrder',
-            trade_type: 'JSAPI',
-            openid: openid           
-        };
-        var sign = wechat.paySign(data, config.wx.key);
-        data.sign = sign.sign;
-        console.log(data);
-    await wechat.getAuthToken(code, function(openid) {
-        var formData = xml.jsonToXml({
-            xml: data
-        });
-        console.log(formData);
-        let options = {
-            url: 'https://api.mch.weixin.qq.com/pay/unifiedorder',
-            method: 'POST',  
-            body: formData
-        };
-        request.post(options, function(err, res, body) {
-            if(err){
-                console.log(err, 'err========');
-            }else{
-                console.log(body, 'body======支付啊日你吗');
-            }
-        });
-    });
+    
     ctx.render('order');
 };
 
