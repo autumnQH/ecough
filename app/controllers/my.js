5cdb9ccdad37949fbb456618b058ac8cc16e6697
MAC <mac@MACtekiMacBook-Air.local>
2017-08-22 23:35:08 +0800

    1

--- app/controllers/my.js
+++ app/controllers/my.js
@@ -99,7 +99,7 @@ var getUserInfo = async (ctx, next) => {
         let code = ctx.query.code;
         var data = tools.getToken(code);
 
-        data.then(function(data) {
+       await data.then(function(data) {
             data = JSON.parse(data);
             tools.getUserInfo(data.access_token, data.openid).then(function(data) {
                 data = JSON.parse(data);
