bc730b3f74c0c7a30ffd65e8f94c614845590994
MAC <mac@MACtekiMacBook-Air.local>
2017-08-23 21:24:40 +0800

    1

--- app/controllers/my.js
+++ app/controllers/my.js
@@ -102,7 +102,7 @@ var jsapiPay = async(ctx, next) => {
     var str1 = tools.raw(data);
     console.log('这都能卡？');
     str1 += '&key='+ key;
-    var sign = crypto.createHash('md5').update(str1, 'utf8').digest('hex').toUpperCase();//签名
+    var sign = await crypto.createHash('md5').update(str1, 'utf8').digest('hex').toUpperCase();//签名
     console.log('死那里去了？');
     data.sign = sign;
     data = xml.jsonToXml(data);
