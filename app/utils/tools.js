404ccdba8ae0f291a8bde42e544ba1bec34a19a3
MAC <mac@MACtekiMacBook-Air.local>
2017-08-24 02:01:49 +0800

    2

--- app/utils/tools.js
+++ app/utils/tools.js
@@ -30,7 +30,7 @@ exports.raw = function(args){
       keys = keys.sort();
       var newArgs = {};
       keys.forEach(function(key) {
-          newArgs[key.toLowerCase()] = args[key];
+          newArgs[key] = args[key];
       });
 
       var string = '';
