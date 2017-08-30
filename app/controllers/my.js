21333a42544db0535fb101d0900f3586d280b594
MAC <mac@MACtekiMacBook-Air.local>
2017-08-31 03:36:10 +0800

    2

--- app/controllers/my.js
+++ app/controllers/my.js
@@ -151,61 +151,6 @@ var getOrder = async function(ctx, next) {
     });
 }
 
-var admin = async function (ctx, next) {
-    await ctx.render('admin', {
-
-    });
-}
-
-var admin_order = async function(ctx, next) {
-    var data = await wechat.getOrder();
-    await ctx.render('admin_order', {
-        data: data
-    });
-}
-
-var admin_setOrder = async function (ctx, next) {
-    var req = ctx.request.body;
-    req.create_time = moment().format('YYYY-MM-DD HH:mm:ss');
-    wechat.setOrder(req);
-    await ctx.redirect('/product/100001');
-
-
-}
-
-var admin_qrcode = async function(ctx, next) {
-    var data = await wechat.getQRCode();
-    await ctx.render('admin_spread', {
-        datas: data
-    });    
-}
-
-var admin_setQrcode = async function(ctx, next) {
-    var req = ctx.request.body;
-   
-    var token = await dao.getActiveAccessToken();
-    var json = JSON.stringify({
-        'action_name': 'QR_LIMIT_STR_SCENE',
-        'action_info': {
-            'scene': {
-                'scene_str':req.name
-            }
-        }
-    });
-    console.log(json,'json--------');
-    var data = await tools.getQRCode(token, json);
-    console.log(data,'data------');
-    data = JSON.parse(data);
-    data.create_time = moment().format('YYYY-MM-DD HH:mm:ss');
-    data.name = req.name +'';
-    data.scene_str = req.name + '';
-    data.action_name = 'QR_LIMIT_SCENE';
-    data.ticket = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket='+ data.ticket;
-
-    var a = await wechat.setQRCode(data);
-
-    await ctx.redirect('/admin/qrcode');
-}
 module.exports = {
 	//'GET /my/address': getAddress,
     //'GET /my/problem': getProblem,
@@ -216,12 +161,7 @@ module.exports = {
     'GET /notify': notify,
     'POST /notify': notify,
     //'GET /my/userinfo': getUserInfo,
-    'GET /my/pay': jsapiPay,
+    'GET /my/pay': jsapiPay
     //'GET /my/order': getOrder,
-    'GET /admin': admin,
-    'GET /admin/order': admin_order,
-    'POST /admin/setOrder': admin_setOrder,
-    'GET /admin/qrcode': admin_qrcode,
-    'POST /admin/setQrcode': admin_setQrcode
 
 };
