4dd5021962473057c463098650da00acf91d8755
MAC <mac@MACtekiMacBook-Air.local>
2017-09-07 04:55:58 +0800

    2

--- app/controllers/my.js
+++ app/controllers/my.js
@@ -37,8 +37,8 @@ var getProblem= async (ctx, next) => {
 
 
 var getUserInfo = async (ctx, next) => {
+    
     config = await config.config();
-    console.log(config,'白痴啊');
     //console.log('进来啦');
     var r_url = config.server.host + ctx.url;
     var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+ config.wx.appid + 
