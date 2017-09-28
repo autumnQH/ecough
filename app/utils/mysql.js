9daade4dead005c7910e9e6832e7ddb12a95cb0b
MAC <mac@MACtekiMacBook-Air.local>
2017-09-28 16:07:26 +0800

    2

--- app/utils/mysql.js
+++ app/utils/mysql.js
@@ -6,7 +6,7 @@ var options = {
     //'port': config.db.port,
     'database': 'ecough',
     'user': 'root',
-    'password': '450450',
+    'password': 'root',
     'connectionLimit': 5,
     // 'charset': config.db.charset,
     'supportBigNumbers': true,
