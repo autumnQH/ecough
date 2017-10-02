7ce635537a4b57ad81bf905edde723615e89198c
MAC <mac@MACtekiMacBook-Air.local>
2017-10-02 19:33:17 +0800

    2

--- app/utils/test.js
+++ app/utils/test.js
@@ -10,10 +10,10 @@ async function a() {
 	let json = {
 		appid : 'wxff24c10734aed1ef',
     mch_id : '1470073502',
-    out_refund_no : '2017100218574235',//退款号=订单号
-    out_trade_no: '2017100218574235',
-    refund_fee : 100,
-    total_fee : 100
+    out_refund_no : '2017100219142348',//退款号=订单号
+    out_trade_no: '2017100219142348',
+    refund_fee : 2100,
+    total_fee : 2100
 	};
     const nonce_str = tools.createRandom();
     const mch_id = json.mch_id;
