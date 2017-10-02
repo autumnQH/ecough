de5432af7c12a2e05a0664b75cdc1228d6ab85c7
MAC <mac@MACtekiMacBook-Air.local>
2017-10-02 20:50:56 +0800

    2

--- app/middlewares/admin.js
+++ app/middlewares/admin.js
@@ -74,7 +74,7 @@ exports.express = async (ctx)=> {
         //order_count += 1;
     var integral = userinfo.integral;//用户积分
         integral = integral + parseInt(pay_money * config.shoping_integral * 0.01);//计算积分
-
+console.log(integral,'下单送积分------');
     //下单赠送积分
     User.addUserIntegralByOpenId(integral, order_count, openid);
     if(userinfo.flag == '1' && userinfo.eventKey){//首单
