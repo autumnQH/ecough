0ded2f6f3a287e34709cebdf922e5e1e42dcec2b
MAC <mac@MACtekiMacBook-Air.local>
2017-10-02 23:10:32 +0800

    2

--- app/db/db.sql
+++ app/db/db.sql
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS T_WECHAT_USER (
   id int(11) NOT NULL AUTO_INCREMENT,
   openid varchar(128) NOT NULL COMMENT 'openid',
   headimgurl varchar(255) DEFAULT NULL COMMENT '用户头像url',
-  nick varchar(20) DEFAULT NULL COMMENT '用户昵称',
+  nick varchar(50) DEFAULT NULL COMMENT '用户昵称',
   eventKey varchar(128) DEFAULT NULL COMMENT '推广人',
   phone varchar(16) DEFAULT NULL COMMENT '用户电话',
   integral int(16) DEFAULT 0 COMMENT '积分', 
