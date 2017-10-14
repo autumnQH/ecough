b99869800414e751160297ef81535c436c4d1117
MAC <mac@MACtekiMacBook-Air.local>
2017-10-14 23:03:23 +0800

    2

--- app/middlewares/user.js
+++ app/middlewares/user.js
@@ -4,28 +4,12 @@ const pay = require('../utils/pay');
 const tools = require('../utils/tools');
 
 exports.showGift = async (ctx)=> {
-	var a = await User.showGift();
-	ctx.state.data = a;
-	console.log(a)
+	ctx.state.data = await User.showGift();
 	await ctx.render('user/gift');
 }
 
 exports.showGiftById = async (ctx)=> {
-	let id = ctx.params.id;
-	var jsapi_ticket = await dao.getJsapiTicket();
-  var url = 'http://' + ctx.header.host + ctx.url.split('?').slice(0,1);
-  var nonceStr = tools.createRandom();
-  var timeStamp = tools.createTimestamp();
-  var out_trade_no = tools.trade();
-  var value = {
-      nonceStr: nonceStr,
-      timeStamp: timeStamp,
-      out_trade_no: out_trade_no
-  };  
-  
+	let id = ctx.params.id;  
 	ctx.state.data = await User.showGiftById(id);
-	var wxconfig = await pay.setWXConfig(jsapi_ticket, url, value);
-	ctx.state.config = wxconfig;
-	console.log(wxconfig);
 	await ctx.render('user/gift_info');
 }
