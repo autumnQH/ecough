1c9368221469d3f0e9db0dccbba54b8a1858bb09
MAC <mac@MACtekiMacBook-Air.local>
2017-10-16 14:33:33 +0800

    2

--- app/middlewares/admin.js
+++ app/middlewares/admin.js
@@ -71,9 +71,10 @@ exports.order = async (ctx)=> {
 
 //设置发货
 exports.express = async (ctx)=> {
-	var req = ctx.request.body;
+  var req = ctx.request.body;
   var out_trade_no = req.out_trade_no;
   var order = await Admin.getOrderByOutTradeNo(out_trade_no);
+  console.log(order,'??');
   if(order.status == 2 && req.status == 3){//第一次发货
     var openid = order.openid;//openid
     var specifications = order.specifications;//规格
@@ -81,7 +82,7 @@ exports.express = async (ctx)=> {
     var total = Number(order.total.replace(/[\u4e00-\u9fa5]+/g,""));//数量,去除中文。件
 
     var userinfo = await User.getUserByOpenId(openid);//获取用户信息
-    var config = await Config.getActivityCFG();//获取活动信息
+    //var config = await Config.getActivityCFG();//获取活动信息
 
     var order_count = userinfo.order_count;//下单件数
         order_count += total;
