ab5308dd56dae5d022daa98b66df365caac76ca5
MAC <mac@MACtekiMacBook-Air.local>
2017-10-16 15:15:14 +0800

    2

--- app/routers/user.js
+++ app/routers/user.js
@@ -3,7 +3,7 @@ const checkUser = require('../middlewares/').check.checkUser;
 const userController = require('../middlewares').user;
 
 router.get('/order', checkUser, userController.showOrder);
-router.get('/gift', checkUser, userController.showGift);
+router.get('/gift', userController.showGift);
 router.get('/gift/:id', checkUser, userController.showGiftById);
 
 
