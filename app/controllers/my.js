5e335b7294df3df4c10c5ba2c91bae73cae41a5a
MAC <mac@MACtekiMacBook-Air.local>
2017-10-17 16:13:09 +0800

    2

--- app/controllers/my.js
+++ app/controllers/my.js
@@ -11,48 +11,6 @@ const USER = require('../utils/user');
 const STORE = require('../utils/store');
 const logUtil = require('../../utils/log');
 
-//预购
-// var PreOrder = async (ctx, next) => {
-//     const config = await dao.getConfig();
-//     var r_url = config.server_host + ctx.url;
-//     var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+ config.appid + 
-//         '&redirect_uri=' + urlencode(r_url) + '&response_type=code&scope=snsapi_userinfo&state=111#wechat_redirect';
-
-//     if(ctx.session.openid){
-//         await ctx.render('product', {
-//             userinfo: ctx.session,
-//             current_money_13: config.current_money_13 * 0.01,
-//             original_money_13: config.original_money_13 * 0.01,
-//             current_money_10: config.current_money_10 * 0.01,
-//             original_money_10: config.original_money_10 * 0.01
-//         });       
-//     }else{
-//         if(!ctx.query.code){
-//             ctx.redirect(url);
-//         }else{
-//             let code = ctx.query.code;
-//             var user = await tools.getOauth2Token(code);
-//                 user = JSON.parse(user);
-//             if(user.errcode){
-//                 console.log(user.errcode);
-//                 ctx.redirect(url);
-//             }else{
-//                 var userinfo = await tools.getUserInfo(user.access_token, user.openid);
-//                     userinfo = JSON.parse(userinfo);
-//                 ctx.session = userinfo;
-//                 await ctx.render('product', {
-//                     userinfo: userinfo,
-//                     current_money_13: config.current_money_13 * 0.01,
-//                     original_money_13: config.original_money_13 * 0.01,
-//                     current_money_10: config.current_money_10 * 0.01,
-//                     original_money_10: config.original_money_10 * 0.01
-//                 })
-//             }    
-//         }
-//     }
-
-// };
-
 var product = async (ctx, next) => {
     const config = await dao.getConfig();
     var product_id = ctx.params.product;
@@ -177,81 +135,7 @@ var jssdk = async(ctx, next) => {
     }
 
 };
-// //统一下单-生成预付单-获取package
-// var jsapiPay = async(ctx, next) => {
-//     const config = await dao.getConfig();
-//     var openid = ctx.query.openid;
-//     var total = ctx.query.total;//数量
-//     var specifications = ctx.query.specifications;//规格
 
-//     var nonceStr = tools.createRandom();
-//     var timeStamp = tools.createTimestamp();
-//     var out_trade_no = tools.trade();
-
-//     var value = {
-//         nonceStr: nonceStr,
-//         timeStamp: timeStamp,
-//         out_trade_no: out_trade_no
-//     };
-
-//     var current_money = 0;//现价
-//     var derate_money = 0;//首单减免减免
-
-//     var todaySubscribe = await USER.getUserFlagByOpenId(openid); 
-    
-//     if(todaySubscribe.flag == '1'){//首单
-//         derate_money = config.derate_money
-//     }
-
-//     switch(specifications) {
-//         case '1.3米':
-//         current_money = config.current_money_13;
-//         break;
-//         case '1米':
-//         current_money = config.current_money_10;
-//         break;
-//     }
-
-//     var pay_money = total * current_money - derate_money ;
-//     var page = await pay.setPackageData(openid, pay_money, value);
-    
-//     //console.log(page,'统一下单');
-
-//     var res = await tools.getPackge(page);//发起统一下单
-//     var result = await xml.xmlToJson(res);//解析统一下单返回的xml数据
-//     //console.log(result);
-//     if(result.xml.err_code){
-//         if(result.xml.err_code[0] == 'ORDERPAID'){
-//             console.log('该订单已经支付');
-//             await ctx.redirect('/product/100001');
-//         }
-//     }
-//     if(result.xml.prepay_id){
-//         var prepayid = result.xml.prepay_id[0];
-//         var data2 = await pay.setPaySign(prepayid, value);
-
-//         //获取js-ticket才能调用微信支付请求
-//         //获取js-ticket
-//         var jsapi_ticket = await dao.getJsapiTicket();
-//         var url = 'http://' + ctx.header.host + ctx.url;
-//         var wxcfg = await pay.setWXConfig(jsapi_ticket, url, value);
-
-//         await ctx.render('order', {
-//             config: wxcfg,
-//             data: data2,
-//             openid: openid,
-//             total: total,
-//             specifications: specifications,
-//             total_money: (pay_money + derate_money) * 0.01,
-//             derate_money: derate_money * 0.01,
-//             pay_money: pay_money * 0.01,
-
-//         });         
-//     }else{
-//         await ctx.redirect('/product/100001');
-//     }
-
-// };
 
 //收款通知
 var notify = async function(ctx, next) {
