e92ff5b520435da668762dc418ca6d2e52d4df05
MAC <mac@MACtekiMacBook-Air.local>
2017-11-06 22:07:57 +0800

    2

--- app/public/js/product.js
+++ app/public/js/product.js
@@ -115,13 +115,16 @@ $(function() {
           jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone']
         });//wx/config end
 
-        wx.checkJsApi({
-            jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-            success: function(res) {
-                alert(res);
-        // 以键值对的形式返回，可用的api值true，不可用为false
-        // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-            }
+        wx.ready(function() {
+            wx.checkJsApi({
+                jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+                success: function(res) {
+                    alert(res);
+            // 以键值对的形式返回，可用的api值true，不可用为false
+            // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+                }
+            });
+            
         });
 
         wx.onMenuShareTimeline({//分享朋友圈
