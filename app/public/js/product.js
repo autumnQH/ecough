030e0d7ee2e90108e6e28adbca040c4be2846b34
MAC <mac@MACtekiMacBook-Air.local>
2017-11-06 22:23:33 +0800

    2

--- app/public/js/product.js
+++ app/public/js/product.js
@@ -121,6 +121,7 @@ $(function() {
             wx.checkJsApi({
                 jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
                 success: function(res) {
+                    alert(news.title);
             // 以键值对的形式返回，可用的api值true，不可用为false
             // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
                 }
