6bb662c2a5ca753df6cbbcc68e757c4581cb389d
MAC <mac@MACtekiMacBook-Air.local>
2017-11-16 20:22:06 +0800

    2

--- app/middlewares/user.js
+++ app/middlewares/user.js
@@ -119,8 +119,11 @@ exports.addService = async (ctx)=> {
 }
 
 exports.showFAQ = async (ctx)=> {
+    console.log('asd?');
     ctx.state.data =  await User.getFAQ();
+    console.log('asd?');
     ctx.state.openid = ctx.session.openid;
+    console.log('asd?11');
     await ctx.render('user/FAQ');
 }
 
