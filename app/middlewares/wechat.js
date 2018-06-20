85a655222d0b8706eb2296168bd19e5d37b6aac1
haiqinJack <1198617472@qq.com>
2018-06-20 18:13:43 +0800

    2

--- app/middlewares/wechat.js
+++ app/middlewares/wechat.js
@@ -3,13 +3,124 @@ const WXSDK = require('../proxy').WXSDK;
 const tools = require('../utils/tools');
 const pay = require('../utils/pay');
 const xml = require('../utils/xml');
+const wechat = require('../utils/wechat');
+const dao = require('../dao/wechat');
+const moment = require('moment');
+const urlencode = require('urlencode');
+const tools = require('../utils/tools');
+const User = require('../proxy').User;
 
-// exports.voucher = async (ctx)=> {
-// 	var openid = ctx.session.openid;
-// 	var voucher = await User.getUserVoucherByOpenId(openid);
-// 	console.log(voucher);
-// 	ctx.body = voucher;
-// }
+
+exports.checkToken = async (ctx, next) => {
+    let result = await wechat.auth(ctx);
+    if (result) {
+        ctx.body = ctx.query.echostr
+    } else {
+        return false;
+    }
+};
+
+exports.postHandle = async(ctx, next) => {
+    let msg = ctx.request.body ? ctx.request.body.xml : '';
+ 
+    if (!msg) {
+        ctx.body = 'error request.';
+        return;
+    }
+
+    var config = await dao.getConfig();
+    var token = await dao.getActiveAccessToken();
+    var openid = msg.FromUserName[0];
+    var userinfo = await tools.getUserInfo2(token,openid);
+    var data = {
+        openid: openid,
+        create_time: moment(msg.CreateTime[0] * 1000).format('YYYY-MM-DD HH:mm:ss'),
+        headimgurl: userinfo.headimgurl,
+        nick: userinfo.nickname
+    };
+    console.log(msg);
+    
+    let msgType = msg.MsgType[0];
+    if(msg.Event){
+        if(msg.Event[0] == 'subscribe'){//用户关注
+                //记录用户关注信息
+            var result =  await wechat.getOneUser(openid);
+
+            if(result.length == 0){
+                wechat.setUser(data);
+            }else{
+                //修改用户
+                wechat.updateUser(data, {openid: openid});                                 
+            }
+                
+            if(msg.Ticket){
+                if(msg.EventKey[0].replace(/^qrscene_/,"") != openid){//自己扫描自己不算
+                    //记录用户扫描带参数的二维码
+                    var eventKey = msg.EventKey[0].replace(/^qrscene_/,"");
+                    //修改用户备注
+                    tools.updateremark(token, openid, eventKey);                       
+                    data.eventKey= eventKey,
+                    //修改用户
+                    wechat.updateUser(data, {openid: openid});
+                    
+                }                
+            }
+        }else if(msg.Event[0] == 'unsubscribe'){//取消关注
+            //User.removeUserByOpenId(openid);
+            User.removeUserForEventKeyByOpenid(openid);
+        }
+    }
+
+    var reMsg;
+
+
+    switch (msgType) {
+          case 'text':
+            reMsg = wechat.transfer2CustomerService(msg);
+            break;
+          case 'image':
+            reMsg = wechat.transfer2CustomerService(msg);
+            break;
+          case 'voice':
+            reMsg = wechat.transfer2CustomerService(msg);
+            break;
+          case 'event':
+
+            switch (msg.Event[0]) {
+
+              case 'subscribe':
+                msg.MsgType = 'text';
+                reMsg = wechat.getTextMessage(msg, config.message_text);
+                break;
+
+              case 'TEMPLATESENDJOBFINISH':
+                reMsg = await wechat.getTextMessage(msg);
+                break;
+
+              case 'kf_create_session':
+                reMsg = wechat.transfer2CustomerService(msg);
+                break;
+              case 'SCAN':
+                msg.MsgType = 'text';
+                reMsg = wechat.transfer2CustomerService(msg);
+                break;
+              default:
+                reMsg = wechat.getTextMessage(msg, config.message_text);
+                break;
+            }
+
+            break;
+
+          default:
+              reMsg = wechat.transfer2CustomerService(msg);
+            break;
+    }
+
+    console.log("reply message: " + reMsg); 
+    return ctx.body = reMsg;
+     
+};
+
 
 exports.sdk = async (ctx)=> {
     let url = decodeURIComponent(ctx.query.url);
--- app/routers/wechat.js
+++ app/routers/wechat.js
@@ -1,6 +1,8 @@
 const router = require('koa-router')();
 const wechatController = require('../middlewares').wx;
 
+router.get('/', wechatController.checkToken) // 验证微信服务器发送来的消息
+router.post('/', wechatController.postHandle)// 接收并回复微信服务器发来的消息
 router.get('/sdk', wechatController.sdk);//获取网页js-sdk
 router.post('/pay', wechatController.pay);//微信支付
 
