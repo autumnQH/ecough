b48119e3ee12ba65149c2237590e12d1ca160518
haiqinJack <1198617472@qq.com>
2018-07-19 19:40:13 +0800

    update

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 const config = {
   server: {
-    port: 3000 // 程序端口号,不用修改
+    port: 80 // 程序端口号,不用修改
   },
-  SITE_ROOT_URL: 'http://polite-husky-31.localtunnel.me',
+  SITE_ROOT_URL: 'http://www.fafuna.cn',
   db: {
-    host: '127.0.0.1', // ip地址 10.66.218.107
+    host: '10.66.218.107', // ip地址 10.66.218.107
     port:  '3306', 
     database: 'ecough', 
     user: 'root', 
-    password: '450450', // Root123456==
+    password: 'Root123456==', // Root123456==
     supportBigNumbers: true,
     bigNumberStrings: true 
   }
