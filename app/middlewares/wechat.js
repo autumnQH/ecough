ca9b78aae603c9ed8902d4be7c67e1436cb81a65
haiqinJack <1198617472@qq.com>
2018-07-21 20:30:41 +0800

    update wechat-pay

--- app/middlewares/wechat.js
+++ app/middlewares/wechat.js
@@ -126,10 +126,10 @@ exports.signature = async (ctx)=> {
 
 exports.pay = async (ctx)=> {
     const ip = ctx.ip.match(/\d+.\d+.\d+.\d+/)[0];
-    console.log(ip)
-    const { openid, store_name, total_fee} = ctx.request.body
+    console.log(ip,'ip======')
     const config = await Config.getConfig()
     console.log(config,'-----config')
+    const { openid, store_name, total_fee} = ctx.request.body
     try {
         console.log('!!!')
         const order = {
