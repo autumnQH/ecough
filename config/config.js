c690692b83920594e925ea26f6f29cfcf6cb7dcd
haiqinJack <1198617472@qq.com>
2018-07-21 15:08:57 +0800

    update test pay

--- config/config.js
+++ config/config.js
@@ -1,3 +1,6 @@
 const env = process.env.NODE_ENV || 'development'
 
+console.log(process.env.NODE_ENV)
+console.log(typeof process.env.NODE_ENV)
+
 module.exports = Object.assign({}, require(`./${env}`));
