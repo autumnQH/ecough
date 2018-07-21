517f81c944c96c122e16f78cb99e0a156e6cb267
haiqinJack <1198617472@qq.com>
2018-07-21 17:46:07 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       host : '111.230.201.47',
       ref  : 'origin/master',
       repo : 'https://github.com/autumnQH/ecough.git',
-      path : '/home/ecough',
+      path : '/home/ecough/production',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       env: {
         NODE_ENV: 'production'
