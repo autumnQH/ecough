c41159c92a546df6bcebdfc053dd334a87f3be0e
haiqinJack <1198617472@qq.com>
2018-07-21 17:39:22 +0800

    update pm2.config

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [{
+    name      : 'app',
+    script    : 'app.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '111.230.201.47',
+      ref  : 'origin/master',
+      repo : 'https://github.com/autumnQH/ecough.git',
+      path : '/home/ecough/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+};
