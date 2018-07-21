8b04d851bb96a1953534f198b4ca0b8e052ff54f
haiqinJack <1198617472@qq.com>
2018-07-21 14:31:58 +0800

    update test pay

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 const config = {
   server: {
-    port: 3000 // 程序端口号,不用修改
+    port: 80 // 程序端口号,不用修改
   },
-  SITE_ROOT_URL: 'http://fafuna.vipgz1.idcfengye.com',// http://www.fafuna.cn
+  SITE_ROOT_URL: 'http://www.fafuna.cn',// http://www.fafuna.cn //http://fafuna.vipgz1.idcfengye.com
   db: {
-    host: '127.0.0.1', // ip地址 10.66.218.107
+    host: '10.66.218.107', // ip地址 10.66.218.107
     port:  '3306', 
     database: 'ecough', 
     user: 'root', 
-    password: '450450' // Root123456==
+    password: 'Root123456==' // Root123456==
   }
 }
 
