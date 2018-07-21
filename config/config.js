0fc08fba39ce2d94596992f170263b9e991f8767
haiqinJack <1198617472@qq.com>
2018-07-21 14:58:00 +0800

    update test pay

--- config/config.js
+++ config/config.js
@@ -1,15 +1,3 @@
-const config = {
-  server: {
-    port: 3000 // 程序端口号,不用修改
-  },
-  SITE_ROOT_URL: 'http://fafuna.vipgz1.idcfengye.com',// http://www.fafuna.cn //http://fafuna.vipgz1.idcfengye.com
-  db: {
-    host: '127.0.0.1', // ip地址 10.66.218.107
-    port:  '3306', 
-    database: 'ecough', 
-    user: 'root', 
-    password: '450450' // Root123456==
-  }
-}
+const env = process.env.NODE_ENV || 'development'
 
-module.exports = Object.assign({}, config);
+module.exports = Object.assign({}, require(`./${env}`));
---
+++ config/production.js
@@ -0,0 +1,13 @@
+module.exports = {
+  server: {
+    port: 3000 // 程序端口号,不用修改
+  },
+  SITE_ROOT_URL: 'http://www.fafuna.cn',
+  db: {
+    host: '10.66.218.107', // ip地址 10.66.218.107
+    port:  '3306', 
+    database: 'ecough', 
+    user: 'root', 
+    password: 'Root123456==' // Root123456==
+  }  
+}
