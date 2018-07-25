fc54e2482916dbe89a52ed6fdd7265989d259b1e
haiqinJack <1198617472@qq.com>
2018-07-26 00:40:14 +0800

    update order

--- app/dao/order.js
+++ app/dao/order.js
@@ -3,32 +3,32 @@ var db = require("../utils/mysql");
 
 //获取状态非4的订单
 exports.getOrder = () => {
-	return db.find("SELECT * FROM ORDER WHERE status != 4 ORDER BY CREATE_TIME DESC")
+	return db.find("SELECT * FROM ORDERS WHERE status != 4 ORDER BY CREATE_TIME DESC")
 }
 //获取状态是4的订单
 exports.getRefundList = ()=> {
-	return db.find("SELECT * FROM ORDER WHERE status = 4 ORDER BY CREATE_TIME DESC")
+	return db.find("SELECT * FROM ORDERS WHERE status = 4 ORDER BY CREATE_TIME DESC")
 }
 
 //添加订单
 exports.setOrder = (order)=> {
-	return db.add("ORDER", order);
+	return db.add("ORDERS", order);
 }
 //根据id查找订单
 exports.getOneOrderById = (id)=> {
-	return db.findOne("SELECT * FROM ORDER WHERE ?", {id});
+	return db.findOne("SELECT * FROM ORDERS WHERE ?", {id});
 }
 //根据openid 查找用户订单号
 exports.getOrderForTradeByOpenId = (openid)=> {
-	return db.find("SELECT out_trade_no FROM ORDER WHERE ?", {openid});
+	return db.find("SELECT out_trade_no FROM ORDERS WHERE ?", {openid});
 }
 //修改订单
 exports.updateOrderById = (id, data)=> {
-	return db.update("ORDER", data, {id});
+	return db.update("ORDERS", data, {id});
 }
 
 //根据订单号获取用户订单
 exports.getOrderByOutTradeNo = (out_trade_no)=> {
-	return db.findOne("SELECT * FROM ORDER WHERE ? ", {out_trade_no: out_trade_no});
+	return db.findOne("SELECT * FROM ORDERS WHERE ? ", {out_trade_no: out_trade_no});
 }
 
