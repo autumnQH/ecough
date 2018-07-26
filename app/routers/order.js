2e0900a0cbfa22193c617adf13994160a67210cf
haiqinJack <1198617472@qq.com>
2018-07-26 13:36:21 +0800

    update checkUser

--- app/routers/order.js
+++ app/routers/order.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')();
 const orderController = require('../middlewares').order;
+const checkUser = require('../middlewares/').check.checkUser;
 
-router.get('/create', orderController.create); // 创建订单
+router.get('/create', checkUser, orderController.create); // 创建订单
 
 module.exports= router;
--- app/routers/product.js
+++ app/routers/product.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')();
 const productController = require('../middlewares').product;
+const checkUser = require('../middlewares/').check.checkUser;
 
-router.get('/:product', productController.getProductById); // 获取商品
+router.get('/:product', checkUser, productController.getProductById); // 获取商品
 
 module.exports= router;
