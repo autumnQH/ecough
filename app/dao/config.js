a1ce9e1465bb950e435aed58aaab10aa67f6c1a0
haiqinJack <1198617472@qq.com>
2018-07-26 12:59:17 +0800

    update config

--- app/dao/config.js
+++ app/dao/config.js
@@ -6,11 +6,6 @@ exports.getConfig = () => {
 }
 
 //更新config
-exports.saveConfig = async (data)=> {
-	const result = await this.getConfig()
-	if(result.id) {
-		return db.update("CONFIG", data, {id: 1});
-	}else {
-		return db.add("CONFIG", data)
-	}
+exports.saveConfig = (data)=> {
+	return db.update("CONFIG", data, {id: 1});
 }
